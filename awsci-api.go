088406f4db7b776c5a4385ba1de1d95496be7243
Conrad Christensen <conrad.christensen@gmail.com>
2019-08-26 00:45:39 +0200

    added github feature

--- awsci-api.go
+++ awsci-api.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/go-github/v28/github"
 	"log"
 )
 
 func Github(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Printf("request: %+v", request)
+	client := github.NewClient(nil)
+	repos, _, err := client.Repositories.List(context.Background(), "knutster", &github.RepositoryListOptions{})
+	jsonRepos, err := json.Marshal(repos)
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body: "{\"message\": \"Hello ƛ!\"}",
-	}, nil
+		Body: fmt.Sprintf("{\"repos\": %s", jsonRepos),
+	}, err
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go.smartmachine.io/awsci-api
 
 go 1.12
 
-require github.com/aws/aws-lambda-go v1.13.0
+require (
+	github.com/aws/aws-lambda-go v1.13.0
+	github.com/google/go-github/v28 v28.0.0
+)
